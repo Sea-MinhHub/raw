d34db4f9681004c32eea1615c9eb6773b6800e53
Sea-MinhHub <nguyen.hoang.bao.minh.vn@gmail.com>
2025-12-18 10:30:23 +0700

    Update Ex.lua

--- Ex.lua
+++ Ex.lua
@@ -1,4 +1,4 @@
-local Redzmodbyseaminh = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sea-MinhHub/Redz-mod-ui/main/Redz%20mod%20ui"))()
+local Redzmodbyseaminh = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sea-MinhHub/Redz-mod-ui/main/Redz%20mod%20ui%20by%20Sea%20%7C%20Minh%20Hub"))()
 
 local Redzmodbyseaminh = Redzmodbyseaminh:MakeWindow({
     ["Title"] = "Sea | Minh Hub",
